e24903fe7acdd81d412477a0044cbe78f4b05a75
Matt Yurdin <matt.yurdin@gmail.com>
2018-08-06 18:24:46 -0400

    Delete Tests.js

--- Tests.js
+++
@@ -1,5 +0,0 @@
-const test = ( assert ) => {
-  let result = eval(assert) === true ? "passed" : "failed";
-  update_log("Test (" + assert + ") " + result);
-  console.log("Test (" + assert + ") " + result + ".");
-};
