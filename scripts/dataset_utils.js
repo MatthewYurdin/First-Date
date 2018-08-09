3d289eb7e681a3eac4b7497c7163668d69956370
Matt Yurdin <matt.yurdin@gmail.com>
2018-08-08 20:13:18 -0400

    Adds output formatting

--- scripts/dataset_utils.js
+++ scripts/dataset_utils.js
@@ -6,8 +6,16 @@ const descending = (a, b) => b - a;
 
 // RANDOMNESS ------------------------------------------------ //
 
+/**
+ * Returns a random element from an array.
+ * @param {object} arr - The array to randomly select from.
+*/
 const random_element = arr => arr[random_int(arr.length - 1)];
 
+/**
+ * Shuffles an array in-place.
+ * @param {object} arr - The array to shuffle.
+*/
 const shuffle = arr => {
   let i = 0, j = 0, temp = null
   for (i = arr.length - 1; i > 0; i -= 1) {
@@ -20,26 +28,49 @@ const shuffle = arr => {
   return true;
 };
 
-//Return simple random sample of n items from arr
+/**
+ * Returns a random sample of size n from an array.
+ * @param {object} arr - The array to randomly select from.
+ * @param {number} n - The number of cases to select.
+*/
 const random_sample = (arr, n) => {
   let frame = arr.slice(0);
   shuffle(frame);
   return frame.filter((f, index) => index < n);
 };
 
-//Return random integer > 0 and <= max
+/**
+ * Returns a random intenger between zero and max.
+ * @param {number} max - The maximum of the range to select from. Defaults to 1,000,000.
+*/
 const random_int = (max = 1000000) => Number((Math.random() * max).toFixed(0));
 
-//Return random real number > 0 and <= max.
+/**
+ * Returns a random real nuber between zero and max.
+ * @param {number} max - The maximum of the range to select from. Defaults to 1,000,000.
+ * @param {number} precision - The amount of numbers to the right of the decimal point. Defaults to 5.
+*/
 const random_real = (max = 1000000, precision = 5) => Number((Math.random() * max).toFixed(5));
 
 //Returns random boolean assuming specified proportion of true values
+/**
+ * Returns a random boolean value.
+ * @param {number} proportion - The assumed proportion of true value in the population. Defaults to 0.500.
+*/
 const coin_flip = (proportion = 0.5000) => (Math.random() <= proportion) ? true : false;
 
-//Returns either (x + shift) or (x - shift) assuming random selection and 50% chance or each
+/**
+ * Returns either (x - shift) or (x + shift) assuming random selection from a population evenly split between the two.
+ * @param {number} x - The number to randomly add or substract shift from.
+ * @param {number} shift - The number to randomly add or substract from x.
+*/
 const plus_or_minus = (x, shift) => coin_flip() ? x + shift : x - shift;
 
-//Returns array of length N with values selected by mode
+/**
+ * Returns an array of length N and data type (i.e., integer, real, order, rank, ID string, or null value) equal to mode
+ * @param {number} N - The length of the array to generate.
+ * @param {number} mode - The data type with which to fill the array.
+*/
 const dummy_array = (N = 1000, mode = 'empty') => {
   // modes: empty, order, boolean, numbers, words, IDs
   let dummy = [];
@@ -71,6 +102,10 @@ const dummy_array = (N = 1000, mode = 'empty') => {
 
 // COMBINATORICS ------------------------------------------------ //
 
+/**
+ * Returns the factorial of {num}.
+ * @param {number} num - Must be less than 40.
+*/
 const factorial = num => {
   //if less than 20, compute actual factorial
   //if between 20 and 39, compute Stirling approximation
@@ -88,35 +123,67 @@ const factorial = num => {
     return Math.ceil(Math.sqrt(2 * Math.PI * num) * Math.pow((num / Math.E ), num));
   }
   else {
-    throw new Error('factorial() can presently handle only inputs les than 40');
+    throw new Error('factorial() can presently handle only inputs less than 40');
   }
 };
 
+/**
+ * Returns the the number of different combinations of {k} items that can be chosen from {n} total items.
+ * @param {number} n - The number of items to choose from.
+ * @param {number} k - The number of items to choose
+*/
+
 const combinations = (n, k) => factorial(n) / (factorial(k) * factorial(n - k));
 
+/**
+ * Returns the the number of different combinations of {k} not-necessarily-unique items that can be chosen from {n} total items.
+ * @param {number} n - The number of items to choose from.
+ * @param {number} k - The number of items to choose
+*/
 const multicombinations = (n, k) => combinations((n + k - 1), k);
 
+/**
+ * Returns the the number of possible subsets of any length that can be chosen from {n} total items.
+ * @param {number} n - The total number of items to choose from.
+*/
 const subsets = n => Math.pow(2, n);
 
+/**
+ * Returns the the number of different permutations of {k} items that can be chosen from {n} total items.
+ * @param {number} n - The number of items to choose from.
+ * @param {number} k - The number of items to choose
+*/
 const permutations = (n, k) => factorial(n) / factorial(n - k);
 
-//list must be an array
-const sum = (list, key = null) => {
+
+// ARITHMETIC ------------------------------------------------ //
+
+/**
+ * Returns the sum of values in {array} or {array[key]}.
+ * @param {array} arr - May be 1d_array, a 2d_array with {key} equal to index, an array_of_objects with {key} equal to the target property.
+ * @param {string|index} key - Optional. The target index of a 2d_array or the target property name of an array_of_objects
+*/
+const sum = (arr, key = null) => {
 	let value = 0;
-	for (let a = 0; a < list.length; a++ ){
-		value += (key === null) ? list[a] : list[a][key];
+	for (let a = 0; a < arr.length; a++ ){
+		value += (!!key) ? arr[a][key] : arr[a];
+	}
+	return value;
+}
+
+/**
+ * Returns the product of values in {array} or {array[key]}.
+ * @param {array} arr - May be 1d_array, a 2d_array with {key} equal to index, an array_of_objects with {key} equal to the target property.
+ * @param {string|index} key - Optional. The target index of a 2d_array or the target property name of an array_of_objects
+*/
+const product = (arr, key = null) => {
+	let value = 0;
+	for (let a = 0; a < list.length; a++){
+		value *= (!!key) ? arr[a][key] : arr[a];
 	}
 	return value;
 };
 
-//TODO why does this always return zero?
-const product = (list, key = null) => {
-	let value = 0;
-	for (let a = 0; a < list.length; a++){
-		value *= ( key === null ) ? list[a] : list[a][key];
-	}
-	return value;
-};
 
 // DATA INSPECTION, COMBINATION, TRANSFORMATION ----------------/
 
@@ -132,33 +199,51 @@ const product = (list, key = null) => {
 
 */
 
+/**
+ * Returns true of {element} is in {list}, which may be an array or a string.
+ * @param {array|string} list - The array or string in which to search for {element}.
+ * @param {value} element - The value or substring to search {list} for,
+*/
 const includes_element = (list, element) => (list.indexOf(element) > -1);
 
+/**
+ * Returns true {prop} is a property of {list}, which must be an object.
+ * @param {object} list - The object in which to search for property {prop}.
+ * @param {string} prop - The name of a property to search for.,
+*/
 const includes_property = (list, prop) => includes_element(Object.keys(list), prop);
 
+/**
+ * Returns an array of distinct values from array {arr}.
+ * @param {array} arr - The array containing all the non-necessarily-unique values.
+*/
 const distinct_values = (arr) => {
   let result = [];
   for (let i=0; i < arr.length; i++){
     if (arr[i] !== null){
-      
       if (!includes_element(result, arr[i])) result.push(arr[i]);
-    
     }
   }
   return result;
 }
 
-//TODO handle empty places in array
-const count_missing_values = (arr) => {
-  let count = 0;
-  for (let i=0; i < arr.length; i++){
-    if (arr[i] === null) count += 1;
-  }
-  return count;
-}
+/**
+ * Returns number of unique values in array {arr}. This includes null values.
+ * @param {array} arr - The array of all the values.
+*/
+//TODO handle empty places in array?
+const count_missing_values = (arr) => missing_values(arr).length;
 
+/**
+ * Returns true if value {thing} is of type string, number, or boolean.
+ * @param {    } thing - The value to test.
+*/
 const scalar = thing => (/string|number|boolean/).test(typeof thing);
 
+/**
+ * Returns type (string, intenger, real, boolean, mixed) of values in {arr}.
+ * @param {array} arr - The array of values to check for type.
+*/
 const detect_type = (arr) => {
   let types = [];
   for (let i = 0; i < arr.length; i++){
@@ -192,6 +277,11 @@ const detect_type = (arr) => {
   }
 }
 
+/**
+ * Returns useful information of values in {arr}.
+ * @param {array} arr - The array of values to probe.
+ * @param {string|intenger} name - The variable name or array index to give to data in {arr}.
+*/
 const test_variable = (arr, name) => {
   const variable = {};
   variable.name = name;
@@ -252,8 +342,10 @@ const test_object_of_arrays = list => {
   return result;
 }
 
-/*
-    metaadata() returns an object:
+/**
+ * Returns a metadata object See below.
+ * @param {object} dataset - The object to derive metadata from.
+ * metaadata() returns an object:
     
     {"structure":"1d-array"|"2d-array"|"array_of_objects"|"object_of_arrays",
      "valid":true|false,
@@ -292,13 +384,65 @@ const metadata = dataset => {
 
 }
 
-const glue = (top, bottom) => [];
+const common_elements = (arr1, arr2) => {
+  let common = [];
+  for (i in arr1){
+    if (includes_element(arr2, i)) common.push(i);
+  }
+  for (j in arr2){
+    if (includes_element(arr1, j)) common.push(j);
+  }
+  return distinct_values(common);
+}
+
+/**
+ * Returns a new object combining {top} and {bottom}.
+ * @param {array|object} top - The array of values to probe.
+ * @param {array|object} name - The variable name or array index to give to data in {arr}.
+ * TODO limit 2d_array case to equal inner array length; limit array_of_objects case to common properties
+*/
+const glue = (top, bottom, ) => {
+  const top_md = metadata(top);
+  const bottom_md = metadata(bottom);
+  if (top_md.structure === bottom_md.structure){
+    if (includes_element(["1d_array","2d_array","array_of_objects"], top_md.structure)) {
+      return top.concat(bottom);
+    }
+    else if (top_md.structure === "object_of_arrays"){
+      let common = common_elements(top_md.variables.map(a => a.name),bottom_md.variables.map(a => a.name));
+      let result = {};
+      for (c in common){
+        result[c] = (top.map(m => m[c])).concat(bottom.map(m => m[c]));
+      }
+      return result;
+    }
+  } else {
+    throw new Error("glue(): datasets must be of same type");
+  }
+}
 
 const marry = (left, right) => [];
 
-const insert = (list, index) => true;
-
-const remove = (list, element) => true;
+const drop = (dataset, properties = []) => {
+  let meta = metadata(dataset);
+  if (meta.structure === ("object_of_arrays")){
+    for (let i = 0; i < properties.length; i++){
+      dataset.delete(properties[i]);
+    }
+    return true;
+  }
+  else if (meta.structure === ("array_of_objects")){
+    for (let i = 0; i < dataset.length; i++){
+      for (let j = 0; j < properties.length; j++){
+        dataset[i].delete(properties[j]);
+      }
+    }
+    return true;
+  }
+  else {
+    throw new Error("drop(): Input may only be 'object_of_arrays' or 'arrays_of_objects.'");
+  }
+}
 
 // SET OPERATORS --------------------------------/
 
@@ -324,8 +468,8 @@ const difference = ( x, y ) => {
 const save_data = (dataset, file_name) => {
     let file_format = file_name.split(".")[1];
     let mime;
-    if (file_format == 'txt') {
-        mime = 'text/plain';
+    if (file_format == 'json') {
+        mime = 'text/json';
         dataset = JSON.stringify(dataset);
     }
     if (file_format == 'js') {
@@ -334,23 +478,19 @@ const save_data = (dataset, file_name) => {
     }
     else if (file_format == 'csv'){
         mime = 'text/csv';
-        dataset = JSON.stringify(dataset);
+        dataset = print_text(standardize_structure(dataset), "\c");
     }
     else if (file_format == 'tsv'){
-        mime = 'text/csv';
-        dataset = JSON.stringify(dataset);
-    }
-    else if (file_format == 'json') {
-        mime = 'text/json';
-        dataset = JSON.stringify(dataset);
+        mime = 'text/tsv';
+        dataset = print_text(standardize_structure(dataset), "\c");
     }
     else if (file_format == 'R') {
         mime = 'text/plain';
-        dataset = JSON.stringify(dataset);
+        dataset = print_R(standardize_structure(dataset), file_name.split(".")[0]);
     }
     else if (file_format == 'py') {
         mime = 'text/plain';
-        dataset = JSON.stringify(dataset);
+        dataset = print_py(standardize_structure(dataset), file_name.split(".")[0]);
     }
     else {
         throw new Error('Valid output formats are ".js", ".json", ".csv", ".tsv", ".R", or ".py".');
@@ -366,35 +506,96 @@ const save_data = (dataset, file_name) => {
     Log.text += update_log(Date.now(), "User downloaded a copy of a dataset as 'Dataset_" + Log.session + "." + file_format + "'.");
 }
 
-const format_output = (dataset, format) => {
-  if (format === "js") {
-    return "var first_date_data = " + JSON.stringify(dataset) + ";";
+const standardize_structure = dataset => {
+  const result = {};
+  result.meta = metadata(dataset);
+  if (result.meta.structure === "1d_array"){
+    result.standard = dataset.map(m => [m]);
+    return result;
   }
-  else if (format === "json") {
-    return JSON.stringify(dataset);
+  else if (result.meta.structure === "2d_array"){
+    result.standard = dataset;
+    return result;
   }
-  else if (format === "csv") {
-    return print_text(dataset, "\c");
+  else if (meta.structure === "array_of_objects"){
+    let names = meta.variables.map(m => m.name);
+    let result = {};
+    result.standard = [];
+    for (let i = 0; i < dataset.length; i++){
+      let row = [];
+      for (let j = 0; j < names.length; j++){
+        row.push(dataset[i][names[j]]);
+      }
+      result.standard.push(row);
+    }
+    return result;
   }
-  else if (format === "tsv") {
-    return print_text(dataset, "\t");
-  }
-  else if (format === "R") {
-    return print_R(dataset);
-  }
-  else if (format === "py") {
-    return print_py(dataset);
+  else if (meta.structure === "object_of_arrays"){
+    let names = meta.variables.map(m => m.name);
+    result.standard = [];
+    for (let i = 0; i < dataset[names[0]].length; i++){
+      let row = [];
+      for (let j = 0; j < names.length; j++){
+        row.push(dataset[names[j]][i]);
+      }
+      result.standard.push(row);
+    }
+    return result;
   }
 }
 
-const print_text = (d, f) =>{
-
+const print_text = (d, delim) =>{
+  let result = "";
+  let variables = meta.variables.map(m => m.name);
+  result += variables.join(delim) + "\n";
+  for (let i = 0; i < dataset.length; i++){
+    result += d.standard[i].join(delim) + "\n";
+  }
+  return result
 }
 
-const print_r = () =>{
+const print_r = (d, name) =>{
+  let result = "setwd()\n" + name + " <- data.frame(";
+  const types = {"number": "numeric", "integer": "numeric", "string":"character", "boolean":"logical"};
+  let names = d.meta.variables.map(m => [m.name,m.type]);
+  for (let i = 0; i < names.length; i++){
+    result += names[i][0] + " = " + types[d.meta.variables[i].type] + "(" + d.standard.length + ")";
+    if (i < (names.length-1)){
+      result += ", ";
+    }
+    else result += ")\n";
+  }
+  for (let i = 0; i < names.length; i++){
+    result += name + "$" + names[i] + " <- c(";
+    result += d.standard.map(m => m[i]).join(", ") + ")\n";
+  }
+  return result;
+  //Make this:
+  //matty <- data.frame(id = character(3), max1 = numeric(3), max2 = numeric(3))
+  //matty$id <- c("horrible", "terrible", "hopeless")
+  //matty$max1 <- c(1000, 997, 765)
+  //matty$max2 <- c(1000, 997, 765)
   
 }
 
-const print_py = () =>{
-  
+const print_py = (d, name) =>{
+  let result = name + " = {\n";
+  const types = {"number": "numeric", "integer": "numeric", "string":"character", "boolean":"logical", "mixed": "character"};
+  let names = d.meta.variables.map(m => [m.name,m.type]);
+  for (let i = 0; i < names.length; i++){
+    result += ("[" + names[i][0] + ": ");
+    result += d[names[i]].map(m => py_type_conversion(types[d.meta.variables[i].type], m)).join(", ") + "]";
+    if (i < (names.length-1)){
+      result += ",\n";
+    }
+    else result += "\n";
+  }
+  result += "]";
+  return result;
+}
+
+const py_type_conversion = (type, value) => {
+  if (type === "numeric") return (!!value) ? value : "NaN";
+  else if (type === "character") return (!!value) ? value : "";
+  else if (type === "logical") return (value === true) ? "True" : "False";
 }
